6198a822fe26a43c317172b9888ee7444b937b8a
Marko Rodic <mrmarkorodic@googlemail.com>
2017-03-24 01:54:42 +0000

    Simple landing page for personal project

---
+++ app.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+  document.getElementById("more").onclick = function (){
+    window.scrollBy(0,800)
+  }
+  document.getElementById("button").onclick = function (){
+    window.scrollBy(0,1700)
+  }
+}
