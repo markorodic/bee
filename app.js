648c8bfa03507fd3e123166f31e83aa9e1bafc93
Marko Rodic <mrmarkorodic@googlemail.com>
2017-03-26 17:26:45 +0100

    added get in touch link

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 window.onload = function() {
   document.getElementById("more").onclick = function (){
-    window.scrollBy(0,800)
+    window.scrollBy(0,750)
   }
   document.getElementById("button").onclick = function (){
     window.scrollBy(0,1700)
